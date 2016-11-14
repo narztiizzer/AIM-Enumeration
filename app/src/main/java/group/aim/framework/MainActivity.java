9883f9772b9cafabe7f45806c8b788b65a4313ca
narztiizzer <nattapong.poom@gmail.com>
2016-11-14 16:41:31 +0700

    Update something

--- app/src/main/java/group/aim/framework/MainActivity.java
+++ app/src/main/java/group/aim/framework/MainActivity.java
@@ -3,11 +3,21 @@ package group.aim.framework;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import group.aim.framework.enumeration.UpdatePolicy;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        UpdatePolicy policy = UpdatePolicy.ForceUpdate;
+
+        if(policy == UpdatePolicy.ForceUpdate) {
+            // TODO : Something
+        } {
+            // TODO : Nothing
+        }
     }
 }
